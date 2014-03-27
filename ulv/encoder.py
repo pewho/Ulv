180b28ebde9eca2aaae72fb27459248c82c9c155
Pewho <mathias.bazire@sylpheo.com>
2014-03-27 11:14:01 +0100

    add documentation on Encoder Class

--- ulv/encoder.py
+++ ulv/encoder.py
@@ -10,11 +10,39 @@ import collections
 
 
 class Encoder:
+    """
+        Encode to / Decode from a specific encoding <> Unicode.
 
+        :Example:
+        >>> enc = Encoder('latin_1')
+        >>> val = 'test'
+        >>> type(val)
+        <class 'str'>
+        >>> val_encoded = enc.encode_data(val)
+        >>> type(val_encoded)
+        <class 'bytes'>
+        >>> print(val_encoded)
+        b'test'
+        >>> unicode_val = enc.decode_data(val_encoded)
+        <class 'str'>
+        >>> print(unicode_val)
+        test
+    """
     def __init__(self, encoding='utf8'):
+        """
+            :param encoding: encoding used for convertion
+        """
         self.encoding = encoding
 
     def encode_data(self, data):
+        """
+            Encode from unicode to a byteString.
+
+            This convert an unicode string to bytes string, or recursively unicode string on iterable / dict
+            to the selected encoding. Other type is conserved.
+
+            :param data: the string to convert, or an iterable, or a dict
+        """
         if isinstance(data, str):
             return data.encode(self.encoding)
         elif isinstance(data, collections.Mapping):
@@ -25,6 +53,14 @@ class Encoder:
             return data
 
     def decode_data(self, data):
+        """
+        Decode from a byteString to unicode.
+
+        This convert a byte string to unicode, or recursively bytes string on iterable / dict
+        from the selected encoding. Other type is conserved.
+
+        :param data: the string to convert, or an iterable, or a dict
+        """
         if isinstance(data, bytes):
             return data.decode(self.encoding)
         elif isinstance(data, collections.Mapping):
@@ -33,4 +69,3 @@ class Encoder:
             return type(data)(map(self.decode_data, data))
         else:
             return data
-
