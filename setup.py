037211670f70d49ef4044e5ee40fb659ed06c978
Mathias Bazire <mathias.bazire.inops@axa.fr>
2015-03-23 15:39:39 +0100

    add fix to setup

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
         "License :: OSI Approved",
         "Natural Language :: French",
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3",
         "Topic :: Parser",
     ],
     license="MIT"
